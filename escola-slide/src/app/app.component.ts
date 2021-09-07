f3baf190c45de92bc3007d6364f67873b8e98a36
Gustavo Amorim <gustavoframorim@hotmail.com>
2021-09-07 19:04:17 -0300

    feat: Material modules added feat: first main toolbar

--- escola-slide/src/app/app.component.ts
+++ escola-slide/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TooltipPosition } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'escola-slide';
+
+  private _tooltipPosition: TooltipPosition;
+  private _tooltipShowDelay: number;
+  private _tooltipHideDelay: number;
+
+  constructor() {
+
+    this._tooltipPosition = 'after';
+    this._tooltipShowDelay = 600;
+    this._tooltipHideDelay = 600;
+  }
+
+  get tooltipPosition() {
+
+    return this._tooltipPosition;
+  }
+
+  get tooltipShowDelay() {
+
+    return this._tooltipShowDelay;
+  }
+
+  get tooltipHideDelay() {
+
+    return this._tooltipHideDelay;
+  }
+
+  onClick() {
+
+  }
+
+  sair() {
+
+  }
 }
--- escola-slide/src/app/app.module.ts
+++ escola-slide/src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesListComponent,
+    CourseDetailsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+
+    FlexLayoutModule,
+
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

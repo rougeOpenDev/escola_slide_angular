950e4ee6d373af250765a12c3311353981756779
odilonskt <odilon123c@gmail.com>
2025-03-17 20:59:20 -0300

    feat: adicionando alto complite

--- escola-slide/src/app/app.module.ts
+++ escola-slide/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-<<<<<<< HEAD
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbInputModule,
   NbActionsModule,
@@ -19,18 +18,7 @@ import { NbInputModule,
   NbThemeModule,
   NbUserModule,
 } from '@nebular/theme';
-=======
->>>>>>> c1385dbecfc3c6d785ece752b244bf6af36f2ab2
 import { NgxEchartsModule } from 'ngx-echarts';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { CardModule } from 'primeng/card';
-import { StepsModule } from 'primeng/steps';
-import { MenuModule } from 'primeng/menu';
-import { PanelModule } from 'primeng/panel';
-import { SidebarModule } from 'primeng/sidebar';
-import { MenubarModule } from 'primeng/menubar';
-import { ToastModule } from 'primeng/toast';
 import { MessageService, PrimeNGConfig } from 'primeng/api';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -67,14 +55,12 @@ import { TricksLibComponent } from './components/tricks/tricks-lib/tricks-lib.co
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-<<<<<<< HEAD
-
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
     NbButtonModule,
     NbSidebarModule.forRoot(),
     NbActionsModule,
-    NbMenuModule.forRoot(),  // Importando NbMenuModule,
+    NbMenuModule.forRoot(),  
     NbIconModule,
     NbUserModule,
     NbEvaIconsModule,
@@ -82,28 +68,18 @@ import { TricksLibComponent } from './components/tricks/tricks-lib/tricks-lib.co
     NbSelectModule,
     NbStepperModule,
     NbInputModule,
-=======
->>>>>>> c1385dbecfc3c6d785ece752b244bf6af36f2ab2
     YouTubePlayerModule,
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')
-    }),
-    ButtonModule,
-    TableModule,
-    CardModule,
-    StepsModule,
-    MenuModule,
-    PanelModule,
-    SidebarModule,
-    MenubarModule,
-    ToastModule
+    NgxEchartsModule,
   ],
-  providers: [PrimeNGConfig, MessageService],
+  providers: [
+    PrimeNGConfig, // Mantenha o PrimeNGConfig no array de providers
+    MessageService,
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
   constructor(private primengConfig: PrimeNGConfig) {
-    this.primengConfig.ripple = true;
+    this.primengConfig.ripplae = true; // Aqui estamos configurando a opção ripple
   }
 }
